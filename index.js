73185de5ed0feaf5b5b338d2814c67878ea45df3
Kevin Gravier <kgravier@fusetg.com>
2016-10-13 10:52:26 -0400

    Simple modifications

--- index.js
+++ index.js
@@ -2,14 +2,25 @@
     angular.module('disableBackspaceNavigation', []).run(['$document', disableBackspaceNavigation]);
 
     function disableBackspaceNavigation($document) {
-        $document.on('keydown', function(ev) {
-            var code, isEditable, nodeName;
-            code = ev.which;
-            nodeName = ev.target.nodeName.toLowerCase();
-            isEditable = ev.target.isContentEditable;
-            if (code === 8 && !((nodeName === 'input' || nodeName === 'select' || nodeName === 'textarea') || isEditable)) {
-                return ev.preventDefault();
-            }
-        });
+        $document.on('keydown', onKeyDown);
+    }
+
+    function onKeyDown(ev) {
+        var code, target;
+        code = ev.which;
+        target = ev.target;
+
+        if (code === 8 && !isInputEl(target)) {
+            ev.preventDefault();
+        }
+
+    }
+
+    function isInputEl(el) {
+        var isEditable, nodeName;
+        isEditable = el.isContentEditable;
+        nodeName = el.nodeName;
+
+        return isEditable || nodeName === 'input' || nodeName === 'select' || nodeName === 'textArea';
     }
 })(window.angular);
